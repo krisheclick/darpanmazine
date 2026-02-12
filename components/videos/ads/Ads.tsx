453b099ea30fccdd671b99605d73b82021f888e9
Rajibul <rajibul.seikh@eclick.co.in>
2026-02-12 18:56:37 +0530

    Footer conflicts

--- components/videos/ads/Ads.tsx
+++ components/videos/ads/Ads.tsx
@@ -1,8 +1,6 @@
 import MobileAd from "@/components/common/advertiesment/poster-ad/MobileAd";
 import MusicAd from "@/components/common/advertiesment/poster-ad/MusicAd";
 import TrendingPostAd from "@/components/common/advertiesment/poster-ad/TrendingPostAd";
-import PosterAd from "@/components/common/advertiesment/poster-ad/PosterAd";
-import BlogAd from "@/components/common/advertiesment/poster-ad/BlogAd";
 import FeaturedVideoGalleries from "./FeaturedGalleries";
 const VideosAds = () => {
     return (
@@ -11,8 +9,6 @@ const VideosAds = () => {
             <MusicAd />
             <FeaturedVideoGalleries />
             <TrendingPostAd />
-            <PosterAd />
-            <BlogAd />
         </>
     )
 }
